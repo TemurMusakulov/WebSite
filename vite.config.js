2368c4e5915466d5c483525739db786af34f415f
TemurMusakulov <temurmusakulov007@gmail.com>
2026-08-20 02:29:35 +0500

    Add Github Actions deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/WebSite/',
   plugins: [react()],
   server: {
     host: true,
